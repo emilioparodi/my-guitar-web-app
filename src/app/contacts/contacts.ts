90cca809ea8500f7095429b98a80d5aa545704c4
El Mariachi <emilioparodi@Boom-shakalaka-3.local>
2025-09-27 00:29:13 -0500

    layout contact form 2

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './contacts.html',
   styleUrl: './contacts.css',
   changeDetection: ChangeDetectionStrategy.OnPush
